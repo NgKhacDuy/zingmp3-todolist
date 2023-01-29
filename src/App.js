17de5d86986ad8ef24c256962207bdf1ea9ea145
khacduy <2002nguyenkhacduy@gmail.com>
2023-01-29 14:22:23 +0700

    get banner

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Routes, Route } from "react-router-dom";
 import path from './ultis/path';
+import { useEffect } from 'react';
+import * as actions from './store/actions'
+import { useDispatch } from 'react-redux';
 
 function App() {
+const dispatch = useDispatch()
+  useEffect(()=>{
+    dispatch(actions.getHome())
+  },[])
   return (
     <>
       <div className="">
--- src/store/reducers/appReducer.js
+++ src/store/reducers/appReducer.js
@@ -1,12 +1,15 @@
 import actionTypes from "../actions/actionTypes";
 const initState = {
-    homeData: [],
-    test: 123
+    banner: [],
+
 }
 const appReducer = (state = initState, action) => {
     switch(action.type){
         case actionTypes.GET_HOME:
-            return state
+            return {
+                ...state,
+                banner: action.homeData?.find(item => item.sectionType === 'banner')?.items || null
+            }
             break;
         default:
             return state
